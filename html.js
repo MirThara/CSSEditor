d125f5b66fd915986f6ac413aeead0067c66441a
MirThara <mi.tha.14@icloud.com>
2025-12-07 19:04:27 +0100

    added example HTML-Page

---
+++ html.js
@@ -0,0 +1,67 @@
+let exampleHTML = `
+    <h1>Hallo Welt!</h1>
+    <p>Dies ist eine Beispielwebsite mit weltbewegendem Inhalt</p>
+
+    Inhalt:
+    <ul>
+        <li><a href="test.html">Test</a></li>
+        <li><a href="HOME.html">HOME</a></li>
+        <li><a href="https://www.mcg-dresden.de">MCG</a></li>
+        <li><a href="https://lernsax.de">LernSax</a></li>
+    </ul>
+    <ol>
+        <li>Das Wichtigste</li>
+        <li>Das Zweitwichtigste</li>
+        <li>
+            <ol>
+                <li>Das Drittwichtigste Erste</li>
+                <li>Das Drittwichtigste Zweite</li>
+            </ol>
+        </li>
+        <li>
+            Das Unwichtige
+        </li>
+    </ol>
+
+    <div class="div">das ganz besonders wichtige DIV</div>
+    <input type="text" name="test" id="test" placeholder="die gefährliche Texteingabe">
+    <button type="submit">Der SUBMIT-BUTTON</button>
+
+    <p>
+        Der inhaltsschwangere Paragraph: <br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae unde vero sapiente quas delectus nemo nostrum!
+        Odit <span class="gold-text">voluptatum</span> debitis commodi, unde beatae ipsam! Numquam cum neque sed quo
+        nisi. Eveniet consectetur tenetur
+        perspiciatis quam perferendis quisquam labore cupiditate temporibus placeat. Sequi nobis, earum porro quidem
+        necessitatibus est quos <b>voluptatum</b> ipsa!
+    </p>
+
+    <audio src="test.mp3" controls>
+        Diese Datei kann leider nicht geladen werden, weil das Chaos gesiegt hat!
+    </audio>
+    <video src="test.mp4" controls>
+        Diese Datei kann leider nicht geladen werden, weil das Chaos gesiegt hat!
+    </video>
+
+    <button>Nicht klicken!</button>
+
+    <table>
+        <thead>
+            <th>Test</th>
+            <th>Test2</th>
+            <th>Test3</th>
+            <th>Test4</th>
+        </thead>
+        <tr>
+            <td>Ergebnis</td>
+            <td>Ergebnis2</td>
+            <td>Ergebnis3</td>
+            <td>Ergebnis4</td>
+        </tr>
+        <tr>
+            <td>Ergebnis2.1</td>
+            <td>Ergebnis2.2</td>
+            <td>Ergebnis2.3</td>
+            <td>Ergebnis2.4</td>
+        </tr>
+    </table>`
